dd5ef0bf66d1b03652de62d61c63eacd9b1c3659
Rafael Batista <raphab33@hotmail.com>
2022-03-17 19:38:54 -0300

    Add login with firebase and create Auth Hook

--- ui/src/config/firebase.config.tsx
+++ ui/src/config/firebase.config.tsx
@@ -1,6 +1,13 @@
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
 import { firebaseConfigEnv } from './env'
+import {
+  getAuth
+} from 'firebase/auth'
+
+import {
+  getFirestore
+} from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: firebaseConfigEnv.apiKey,
@@ -15,3 +22,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
+export const auth = getAuth(app)
+export const db = getFirestore(app)
