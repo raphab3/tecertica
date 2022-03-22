357917046509b52e6452f05b35ff105f82b4bfd1
Rafael Batista <raphab33@hotmail.com>
2022-03-21 21:26:00 -0300

    Update develop

--- ui/src/pages/Home/index.tsx
+++ ui/src/pages/Home/index.tsx
@@ -74,6 +74,8 @@ export const Home = () => {
   const typesAccept = ['image/png', 'image/jpg', 'image/jpeg']
 
   function isSelected() {
+    console.log('teste')
+
     if (shapes.length && isDown !== -1) {
       const shapesForSelect = shapes.map(shape => {
         (shape.index === isDown) ? shape.lineWidth = 4 : shape.lineWidth = 0
@@ -327,7 +329,7 @@ export const Home = () => {
                   <Canvas shapes={shapes} setShapes={setShapes} preview={preview} setIsDown={setIsDown} isDown={isDown} />
                   <Box borderRadius={10} boxShadow={'dark-lg'} bgImg={`url(${require('../../assets/images/background.png')})`} display={'flex'} justifyContent={'space-between'} alignContent={'space-between'} flexDir={'column'} >
                     <Box display={'flex'} width={screen.width / 9} flexDirection={'column'}>
-                      <PreviewCertificate imgPreview={preview} shapes={shapes} isDown={isDown} jsonClients={jsonClients}/>
+                      <PreviewCertificate imgPreview={preview} shapes={shapes} isDown={isDown} jsonClients={jsonClients} />
                       <Button title={(isDown === -1) ? 'selecione um campo pra ser removido' : ''} disabled={(isDown === -1)} _hover={{ boxShadow: '10px 5px 5px black' }} colorScheme='teal' variant='solid' margin={5} type="button" onClick={removerInput}>Remover {screen.width < 600 ? '' : 'Campo'}</Button>
                     </Box>
                     <Box
