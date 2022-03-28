3d7ef036785aa854f4596fe63f1aed40e39de206
Rafael Batista <raphab33@hotmail.com>
2022-03-28 09:24:23 -0300

    Revert "Create util abbreviate"

--- server/scripts/dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#sh scripts/base.sh
-
-echo 'Starting development environment ...';
-yarn dev;
--- server/src/modules/manager/infra/mongodb/repositories/IManagersRepository.ts
+++
@@ -1,7 +0,0 @@
-import ICreateManagersDto from '@modules/manager/dtos/ICreateManagersDto'
-import IManagersDto from '@modules/manager/dtos/IManagersDto'
-
-export default interface IManagersRepository {
-  create(data: ICreateManagersDto): Promise<IManagersDto>
-  find(): Promise<Array<IManagersDto>>
-}
